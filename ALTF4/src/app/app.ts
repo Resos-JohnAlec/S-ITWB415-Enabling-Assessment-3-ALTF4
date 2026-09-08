2fa30d2350ee29825467c43c94a830f46540afed
Resos-JohnAlec <johnalecresos@gmail.com>
2026-09-08 16:01:29 +0800

    feat-resos1 - add - resoscomponent

--- ALTF4/src/app/app.ts
+++ ALTF4/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ResosComponent } from './resos-component/resos-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ResosComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
