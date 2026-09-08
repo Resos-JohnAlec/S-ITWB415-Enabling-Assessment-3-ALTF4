21feab4564ae29fc7ce6cb47213f9540d31fc74a
Magno-BiancaLauryn <biancamagno09@gmail.com>
2026-09-08 23:55:34 +0800

    Add magno components

--- ALTF4/src/app/app.ts
+++ ALTF4/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ResosComponent } from './resos-component/resos-component';
 import { RevillasComponent } from './revillas-component/revillas-component';
 import { LauronComponent } from './lauron-component/lauron-component';
+import { MagnoComponent } from './components/magno-component/magno-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ResosComponent, RevillasComponent, LauronComponent],
+  imports: [RouterOutlet, ResosComponent, RevillasComponent, LauronComponent, MagnoComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
